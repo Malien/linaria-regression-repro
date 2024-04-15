ab63d893d2e540dcb0e69095986422f8aa183749
Yaroslav <q.link0.p@gmail.com>
2024-04-16 01:32:11 +0300

    removed react

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
 import linaria from "@linaria/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    linaria({ babelOptions: { presets: ["@babel/preset-react"] } }),
-  ],
+  plugins: [linaria()],
 })
