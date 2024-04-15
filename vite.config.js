65ea10adf74f30f946553f96d3abadd61404e501
Yaroslav <q.link0.p@gmail.com>
2024-04-16 01:27:50 +0300

    previous version does not exibit the issue

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import wyw from "@wyw-in-js/vite"
+import linaria from "@linaria/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    wyw({ babelOptions: { presets: ["@babel/preset-react"] } }),
+    linaria({ babelOptions: { presets: ["@babel/preset-react"] } }),
   ],
 })
